3acad2e68aa7945b11853890aba218682a76df3e
Julian Padilla <j.padilla@uniandes.edu.co>
2021-11-30 23:17:37 +0000

    Despliegue completo en SAP BTP

---
+++ webapp/controller/DetailPage2.controller.js
@@ -0,0 +1,304 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.prototipoDeAltaFidelidad.controller.DetailPage2", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App6190512eed0c121ed2b5c976";
+
+			var oParams = {};
+
+			this._oRootView = this.getOwnerComponent().getAggregation("rootControl");
+			this._oRootView.getController().setMode(sap.m.SplitAppMode.HideMode);
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onPageNavButtonPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage1", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+
+		},
+		_onButtonPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage3", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress1: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress2: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress3: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress4: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress5: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress6: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress7: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress8: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onGenericTilePress9: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onButtonPress1: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage4", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("DetailPage2").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			var oView = this.getView();
+			oView.addEventDelegate({
+				onBeforeShow: function() {
+					if (sap.ui.Device.system.phone) {
+						var oPage = oView.getContent()[0];
+						if (oPage.getShowNavButton && !oPage.getShowNavButton()) {
+							oPage.setShowNavButton(true);
+							oPage.attachNavButtonPress(function() {
+								this.oRouter.navTo("Dialog1", {}, true);
+							}.bind(this));
+						}
+					}
+				}.bind(this)
+			});
+
+		}
+	});
+}, /* bExport= */ true);
---
+++ webapp/controller/DetailPage4.controller.js
@@ -0,0 +1,170 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./Dialog2",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Dialog2, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.prototipoDeAltaFidelidad.controller.DetailPage4", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App6190512eed0c121ed2b5c976";
+
+			var oParams = {};
+
+			this._oRootView = this.getOwnerComponent().getAggregation("rootControl");
+			this._oRootView.getController().setMode(sap.m.SplitAppMode.HideMode);
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onPageNavButtonPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage2", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+
+		},
+		_onObjectListItemPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("DetailPage2", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onButtonPress: function(oEvent) {
+
+			var sDialogName = "Dialog2";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+			if (!oDialog) {
+				oDialog = new Dialog2(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+
+			var context = oEvent.getSource().getBindingContext();
+			oDialog._oControl.setBindingContext(context);
+
+			oDialog.open();
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("DetailPage4").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			var oView = this.getView();
+			oView.addEventDelegate({
+				onBeforeShow: function() {
+					if (sap.ui.Device.system.phone) {
+						var oPage = oView.getContent()[0];
+						if (oPage.getShowNavButton && !oPage.getShowNavButton()) {
+							oPage.setShowNavButton(true);
+							oPage.attachNavButtonPress(function() {
+								this.oRouter.navTo("DetailPage2", {}, true);
+							}.bind(this));
+						}
+					}
+				}.bind(this)
+			});
+
+		}
+	});
+}, /* bExport= */ true);
